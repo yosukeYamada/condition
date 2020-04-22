8f70618121a4fdf5e3129e310cde5c8d96c19b64
yosukeYamada <yamada_141@yahoo.co.jp>
2020-04-22 13:49:57 +0900

    first commit

---
+++ src/main/java/com/example/ConditionApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionApplication.class, args);
+	}
+
+}
