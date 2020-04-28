e2fe62188a1e42634901fe0ab48151ea00f152e1
suzuki-ryouhei <ryohei.suzuki@rakus-partners.co.jp>
2020-04-28 15:52:58 +0900

    RegisterDailyPost registration

---
+++ src/main/java/com/example/controller/RegisterDailyPostController.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registerDailyPost")
+public class RegisterDailyPostController {
+	public void registerDailyPost() {
+		//フォームの値をサービスに送る
+	}
+}
