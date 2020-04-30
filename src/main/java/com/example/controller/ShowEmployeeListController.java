89168f2f7e4955d55795a63498252ceef6ba2f16
yuichiyasui <yassan.b.a.w@gmail.com>
2020-04-30 12:01:45 +0900

    従業員一覧表示のサービスクラスに日付の引数を渡すように変更

--- src/main/java/com/example/controller/ShowEmployeeListController.java
+++ src/main/java/com/example/controller/ShowEmployeeListController.java
@@ -2,13 +2,14 @@ package com.example.controller;
 
 import java.util.List;
 
+import com.example.domain.User;
+import com.example.service.ShowEmployeeListService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.domain.User;
-import com.example.service.ShowEmployeeListService;
 
 /**
  * 従業員一覧画面を表示するコントローラー.
@@ -22,10 +23,16 @@ public class ShowEmployeeListController {
 	@Autowired
 	private ShowEmployeeListService showEmployeeListService;
 
+	/**
+	 * 
+	 * @return 従業員一覧
+	 */
+	@CrossOrigin(origins="http://localhost:8888") // TODO URLドメインをデプロイ時に変更
 	@ResponseBody
 	@RequestMapping("/showEmployeeList")
 	public List<User> showEmployeeList() {
-		return showEmployeeListService.showEmployee();
+		String date = "2020/04/27";
+		return showEmployeeListService.showEmployee(date);
 	}
 
 }
--- src/main/java/com/example/service/ShowEmployeeListService.java
+++ src/main/java/com/example/service/ShowEmployeeListService.java
@@ -1,17 +1,21 @@
 package com.example.service;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.example.domain.Condition;
 import com.example.domain.DailyPost;
 import com.example.domain.User;
 import com.example.mapper.UserMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 
@@ -28,21 +32,21 @@ public class ShowEmployeeListService {
 	private UserMapper userMapper;
 
 	/**
-	 * ユーザー情報を取得する.
+	 * 従業員情報の一覧を取得するメソッド.
 	 * 
-	 * @return
+	 * @param date 日付('yyyy/MM/dd')
+	 * @return 引数で受け取った日付の投稿を含む従業員一覧
 	 */
-	public List<User> showEmployee() {
-		/**
-		 * employeeListに従業員一覧画面に必要な、
-		 * userName,depName,hireDate,condition,motivation,performance,commentをつめる
-		 */
-
+	public List<User> showEmployee(String date) {
+		LocalDateTime arg = stringToLocalDateTime(date); // 引数で受け取ったyyyy/MM/ddをLocalDateTimeに変換
+        LocalDateTime startLdt = LocalDateTime.of(2020, arg.getMonth(), arg.getDayOfMonth(), 0, 0, 0); // 検索の開始日時を生成
+        LocalDateTime endLdt = LocalDateTime.of(2020, arg.getMonth(), arg.getDayOfMonth(), 23, 59, 59); // 検索の終了日時を生成
+        Timestamp startTs = Timestamp.valueOf(startLdt); // LocalDateTime→Timestamp
+        Timestamp endTs = Timestamp.valueOf(endLdt); // LocalDateTime→Timestamp
+		List<User> findAllList = userMapper.findAllAndDailyPost(); // TODO 日付の引数を渡すように変更する
+		
+		// employeeListに従業員一覧画面に必要な、userName,depName,hireDate,condition,motivation,performance,commentをつめる
 		List<User> employeeList = new ArrayList<User>();
-
-		List<User> findAllList = userMapper.findAllAndDailyPost();
-		
-		
 		User user = new User();
 		DailyPost dailyPost = new DailyPost();
 		List<DailyPost> dailyPostList = new ArrayList<DailyPost>();
@@ -63,6 +67,23 @@ public class ShowEmployeeListService {
 		employeeList.add(user);
 
 		return employeeList;
+	}
+
+	/**
+	 * 日付をStringからLocalDateTimeに変換するメソッド
+	 * 
+	 * @param date 日付('yyyy/MM/dd')
+	 * @return LocalDateTimeに変換した日付
+	 */
+	public LocalDateTime stringToLocalDateTime(String date) {
+		try {
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+			return LocalDate.parse(date, dtf).atTime(LocalTime.MIN);
+		} catch (Exception e) {
+			/** 変換に失敗した場合は今日の日付を返す */
+			e.printStackTrace();
+			return LocalDateTime.now();
+		}
 	}
 
 }
