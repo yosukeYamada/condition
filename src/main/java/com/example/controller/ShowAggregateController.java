86b446575f7364a89f68a354084dff167925180e
yuichiyasui <yassan.b.a.w@gmail.com>
2020-04-30 11:20:30 +0900

    CORSの設定を一時的に許可

--- src/main/java/com/example/controller/ShowAggregateController.java
+++ src/main/java/com/example/controller/ShowAggregateController.java
@@ -7,6 +7,7 @@ import com.example.service.AggregateByDayService;
 import com.example.service.AggregateByMonthService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,13 +34,14 @@ public class ShowAggregateController {
      * @param date 日付('yyyy/MM/dd')
      * @return 引数で受け取った日の集計結果
      */
+    @CrossOrigin(origins="http://localhost:8888") // TODO URLドメインをデプロイ時に変更
     @ResponseBody
     @RequestMapping("/getAggregateByDay")
     public Map<String, DailyScore> getAggregateByDay(@RequestBody @RequestParam("date") String date) {
         // date = "2020/04/27"; // TODO 日付固定用なのでフロントができたら消す
         return aggregateByDayService.aggregateByDay(date);
     }
-
+    
     /**
      * 月別グラフの集計結果を返すメソッド
      * 呼び出し方：http://localhost:8080/getAggregateByMonth?date=2020/04/27
@@ -47,11 +49,12 @@ public class ShowAggregateController {
      * @param date 日付('yyyy/MM/dd')
      * @return 引数で受け取った日付の月の集計結果
      */
+    @CrossOrigin(origins="http://localhost:8888") // TODO URLドメインをデプロイ時に変更
     @ResponseBody
     @RequestMapping("/getAggregateByMonth")
     public Map<String, Map<String, DailyScore>> getAggregateByMonth(@RequestBody @RequestParam("date") String date) {
         // date = "2020/04/27"; // TODO 日付固定用なのでフロントができたら消す
         return aggregateByMonthService.aggregateByMonth(date);
     }
-
+    
 }
