004dde61fc95136e7a8bab85d33cd2533ab6cbc9
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-04-30 22:38:37 +0900

    sysout追加

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +21,6 @@ public class UserController {
 	@Autowired
 	private MailService mailService;
 	
-	@CrossOrigin(origins="http://localhost:8888")
 	@PostMapping("/registerUser")
 	public void registerUser(@RequestBody(required = false) RegisterUserForm form) {
 		User user = registerUserService.registerUser(form);
