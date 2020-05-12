f5efc512ca9ba18c47fcea5437a891ca9f8142cc
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-05-12 14:27:34 +0900

    管理者情報付与

--- src/main/java/com/example/controller/ChangeAuthorityController.java
+++ src/main/java/com/example/controller/ChangeAuthorityController.java
@@ -3,13 +3,14 @@ package com.example.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.example.service.ChangeAuthorityService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.domain.User;
+import com.example.service.ChangeAuthorityService;
 
 /**
  * ユーザー権限を変更するコントローラークラス
@@ -31,13 +32,21 @@ public class ChangeAuthorityController {
     @ResponseBody
     @RequestMapping("/changeAuthority")
     public Map<String, String> changeAuthority(@RequestBody Map<String, String> param) {
-        String name = changeAuthorityService.changeAuthority(param.get("email"),
-                Integer.parseInt(param.get("authority")), Integer.parseInt(param.get("updateUserId")));
-        Map<String, String> resultMap = new HashMap<>();
-        resultMap.put("email", param.get("email"));
-        resultMap.put("name", name);
-        System.out.println("返却するmap:" + resultMap);
-        return resultMap;
+    	
+    	Map<String, String> resultMap = new HashMap<>();
+    	
+    	User user = changeAuthorityService.findUserByMail(param.get("email"));
+    	
+    	if(user.getAuthority() == 3) {
+    		resultMap.put("email", "null");
+    		return resultMap;
+    		
+    	} else {
+    		String name = changeAuthorityService.changeAuthority(param.get("email"),
+    				Integer.parseInt(param.get("authority")), Integer.parseInt(param.get("updateUserId")));
+    		resultMap.put("email", param.get("email"));
+    		resultMap.put("name", name);
+    		return resultMap;
+    	}
     }
-
 }
