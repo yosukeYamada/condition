edebcd0a5050906989848c86f3719a5c949d0744
yuichiyasui <yassan.b.a.w@gmail.com>
2020-05-12 12:48:33 +0900

    未使用のインポートがあったので削除

--- src/main/java/com/example/controller/ShowAggregateController.java
+++ src/main/java/com/example/controller/ShowAggregateController.java
@@ -9,7 +9,6 @@ import com.example.service.AggregateByMonthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
