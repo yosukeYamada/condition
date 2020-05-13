b31866765ae58c261d6fe12921a74efc51a68d40
yuichiyasui <yassan.b.a.w@gmail.com>
2020-05-13 21:17:34 +0900

    npm関係が混ざってたので削除

--- src/main/java/com/example/controller/ChangeAuthorityController.java
+++ src/main/java/com/example/controller/ChangeAuthorityController.java
@@ -20,50 +20,49 @@ import com.example.service.ChangeAuthorityService;
 @RestController
 public class ChangeAuthorityController {
 
-    @Autowired
-    private ChangeAuthorityService changeAuthorityService;
+	@Autowired
+	private ChangeAuthorityService changeAuthorityService;
 
-    /**
-     * ユーザー権限の変更を行うメソッド
-     * 
-     * @param param メールアドレス、変更するユーザー権限、更新ユーザーのID
-     * @return 変更したユーザーのメールアドレスと名前
-     */
-    @ResponseBody
-    @RequestMapping("/changeAuthority")
-    public Map<String, String> changeAuthority(@RequestBody Map<String, String> param) {
-    	
-    	Map<String, String> resultMap = new HashMap<>();
-    	
-    	User user = changeAuthorityService.findUserByMail(param.get("email"));
-    	
-    	if(user.getAuthority() == 3) {
-    		
-    		resultMap.put("email", "null");
-    		return resultMap;
-    		
-    	} else {
-    		
-    		//DBにあるuserのversion番号を取得.
-    		Integer version = user.getVersion();
-    		
-    		System.err.println("DBのversion番号 : " + version);
-    		System.err.println("送られてきたversion番号 : " + Integer.parseInt(param.get("version")));
-    		
-    		
-    		//DBにあるuserのversion番号とフロントから送られてきたversion番号を確認.
-    		if(version != Integer.parseInt(param.get("version"))) {
-    			resultMap.put("version", "null");
-    			return resultMap;
-    			
-    		} else {
-    			
-	    		String name = changeAuthorityService.changeAuthority(param.get("email"),
-	    				Integer.parseInt(param.get("authority")), Integer.parseInt(param.get("updateUserId")));
-	    		resultMap.put("email", param.get("email"));
-	    		resultMap.put("name", name);
-	    		return resultMap;
-    		}
-    	}
-    }
+	/**
+	 * ユーザー権限の変更を行うメソッド
+	 * 
+	 * @param param メールアドレス、変更するユーザー権限、更新ユーザーのID
+	 * @return 変更したユーザーのメールアドレスと名前
+	 */
+	@ResponseBody
+	@RequestMapping("/changeAuthority")
+	public Map<String, String> changeAuthority(@RequestBody Map<String, String> param) {
+
+		Map<String, String> resultMap = new HashMap<>();
+
+		User user = changeAuthorityService.findUserByMail(param.get("email"));
+
+		if (user.getAuthority() == 3) {
+
+			resultMap.put("email", "null");
+			return resultMap;
+
+		} else {
+
+			// DBにあるuserのversion番号を取得.
+			Integer version = user.getVersion();
+
+			System.err.println("DBのversion番号 : " + version);
+			System.err.println("送られてきたversion番号 : " + Integer.parseInt(param.get("version")));
+
+			// DBにあるuserのversion番号とフロントから送られてきたversion番号を確認.
+			if (version != Integer.parseInt(param.get("version"))) {
+				resultMap.put("version", "null");
+				return resultMap;
+
+			} else {
+
+				String name = changeAuthorityService.changeAuthority(param.get("email"),
+						Integer.parseInt(param.get("authority")), Integer.parseInt(param.get("updateUserId")));
+				resultMap.put("email", param.get("email"));
+				resultMap.put("name", name);
+				return resultMap;
+			}
+		}
+	}
 }
