0778cebf56042f1ea89691a648cb6a4233db1990
SakaiAkihiro <akihiro.sakai@rakus-partners.co.jp>
2020-05-14 17:39:17 +0900

    お知らせ投稿の全件取得用を作成

--- src/main/java/com/example/controller/NewsController.java
+++ src/main/java/com/example/controller/NewsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.domain.PostedNews;
@@ -28,13 +29,24 @@ public class NewsController {
 	/**
 	 * お知らせ投稿を行うメソッド.
 	 * 
-	 * @param form
-	 * @return
+	 * @param form 投稿内容
+	 * @return お知らせ投稿一覧
 	 */
 	@RequestMapping("/news")
 	public List<PostedNews> postedNews(@RequestBody PostedNewsForm form) {
 		List<PostedNews> PostedNewsList = postedNewsService.PostedNews(form);
 		return PostedNewsList;
+	}
+	
+	/**
+	 * お知らせ投稿一覧を表示するメソッド.
+	 * 
+	 * @return お知らせ投稿一覧
+	 */
+	@ResponseBody
+	@RequestMapping("/showNewsPostList")
+	public List<PostedNews> showNewsPostList() {
+		return postedNewsService.showNewsPostList();
 	}
 	
 }
