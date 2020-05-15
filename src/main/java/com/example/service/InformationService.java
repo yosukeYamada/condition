8d5d06296288c3e81796b89f090d3188b9260fc0
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-05-15 21:33:46 +0900

    news投稿機能

--- src/main/java/com/example/service/InformationService.java
+++ src/main/java/com/example/service/InformationService.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.domain.Category;
 import com.example.domain.Information;
 import com.example.domain.response.Form;
+import com.example.form.RegisterInformationForm;
 import com.example.mapper.CategoryMapper;
 import com.example.mapper.InformationMapper;
 
@@ -40,5 +42,30 @@ public class InformationService {
 		form.setInformationList(informationList);
 		form.setCategory(category);
 		return form;
+	}
+	
+	/**
+	 * インフォメーションを登録する.
+	 * 
+	 * @param form form
+	 * @return インフォメーション一覧
+	 */
+	public List<Information> insert(RegisterInformationForm form) {
+		
+		Timestamp tsDate = new Timestamp(System.currentTimeMillis()); // 現在時刻を生成
+		
+		Information information = new Information();
+		information.setInformationTitle(form.getTitle());
+		information.setInformationContent(form.getContent());
+		information.setCategoryId(form.getCategoryId());
+		information.setInformationDate(tsDate);
+		information.setRegisterUserId(form.getRegisterUserId());
+		information.setRegisterDate(tsDate);
+		information.setVersion(1);
+		information.setStatus(1);
+		
+		informationMapper.insert(information);
+		
+		return informationMapper.findAll();
 	}
 }
