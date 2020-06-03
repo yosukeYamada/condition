1eefb59d1a29bd382b82ab6948dc2336a47cc9c2
IidaShuhei <shuhei.iida@rakus-partners.co.jp>
2020-06-04 07:55:02 +0900

    ラクスカル君採点修正

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -41,12 +41,12 @@ public class TestController {
 	//50人分をテスト
 	@RequestMapping("/insert")
 	public void insert() {
-		user.insertUsers();
-		mail.insertMails();
-		
-		for (int i = 1; i <= 10; i++) {
-			daily.insertDailyPost(i);
-		}
+//		user.insertUsers();
+//		mail.insertMails();
+//		
+//		for (int i = 1; i <= 10; i++) {
+//		}
+		daily.insertDailyPost();
 		con.insertPostedCondition();
 		mot.insertPostedMotivation();
 		per.insertPostedPerformance();
